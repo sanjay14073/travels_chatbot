1e432cc27eeec65f352d6e51066570da74cac716
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2025-06-28 18:35:42 +0530

    Prompt Refinement

--- server/chat/chatbot.py
+++ server/chat/chatbot.py
@@ -19,6 +19,10 @@ class ChatBot:
         You should search for the distance between source and destination using google maps api and then calculate the price.
         In case the user asks for daily prices the prices are 2000rs for mini, 2500rs for sedan and 3000rs for SUV and max kilometers 300 and add a note that additional charges may apply for extra kilometers.
         Additionally add driver charges of 500rs for daily rentals.
+        Also if flight is viable then you can provide the user with the flight details and also add a note that the flight is subject to availability.provide the cheapest flight details.
+        if train is viable then you can provide the user with the train details and also add a note that the train is subject to availability.provide the train details.
+        If the user asks for a hotel then you can provide the user with the hotel details and also add a note that the hotel is subject to availability.provide the cheapest hotel through google hotels api.
+        if the user asks for a itenerary then you can provide the user with the itinerary through google travel api.
         Provide the user with the correct information based on the query.
         If you are not sure about the answer, politely inform the user that you don't have that information and they may contact xwheels at no +91 9223261217.
         ##USER QUERY
